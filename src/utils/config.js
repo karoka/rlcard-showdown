6bdb8b22f80ad2071337a5a29ee0f33273266c7c
jingruxie03 <jingruxie03@kuaishou.com>
2021-06-20 21:10:22 -0700

    update backend server url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 const apiUrl = 'http://127.0.0.1:8000';
-const douzeroDemoUrl = 'http://127.0.0.1:5000';
+const douzeroDemoUrl = 'https://aiplatform-ddz-backend.test.gifshow.com';
 
 export { apiUrl, douzeroDemoUrl };
